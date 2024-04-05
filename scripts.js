66045e7aecb60a1619ce7acc3b99809710c0943d
Kyrylo <kiriloni2k17@gmail.com>
2024-04-05 19:49:24 -0400

    Array of objects implemented for better scalibility

--- scripts.js
+++ scripts.js
@@ -24,16 +24,13 @@
  */
 
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
 // This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
-];
+let shows = [
+    { title: "Fresh Prince of Bel Air", imageUrl:"https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg"},
+    { title: "Curb Your Enthusiasm", imageUrl:"https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg"},
+    { title: "East Los High", imageUrl:"https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg"},
+]
 // Your final submission should have much more data than this, and 
 // you should use more than just an array of strings to store it all.
 
@@ -44,23 +41,11 @@ function showCards() {
     cardContainer.innerHTML = "";
     const templateCard = document.querySelector(".card");
     
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
-
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
-
-        const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
-        cardContainer.appendChild(nextCard); // Add new card to the container
+    for (let i = 0; i < shows.length; i++) {
+        const show = shows[i];
+        const nextCard = templateCard.cloneNode(true);
+        editCardContent(nextCard, show.title, show.imageUrl);
+        cardContainer.appendChild(nextCard);
     }
 }
 
